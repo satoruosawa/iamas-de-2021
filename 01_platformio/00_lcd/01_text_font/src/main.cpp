13598e690c9c8172e51923a57b38361685e20f44
Satoru Osawa <osawa@takram.com>
2021-06-13 02:10:30 +0900

    Update lcd

---
+++ 01_platformio/00_lcd/01_text_font/src/main.cpp
@@ -0,0 +1,12 @@
+#include <M5Stack.h>
+
+void setup() {
+  M5.begin();
+
+  M5.Lcd.setTextSize(1);
+  M5.Lcd.setTextFont(2);  // Font変更、初期値は1
+  M5.Lcd.setCursor(0, 10);
+  M5.Lcd.println("Font 2");
+}
+
+void loop() {}
--- 01_platformio/00_lcd/06_lines/src/main.cpp
+++ 01_platformio/00_lcd/06_lines/src/main.cpp
@@ -10,11 +10,6 @@ void setup() {
     int y = (i + 1) * 40;
     M5.Lcd.drawLine(10, y, 310, y, WHITE);
   }
-  for (int i = 0; i < 7; i++) {
-    // 縦の線を7本描画
-    int x = (i + 1) * 40;
-    M5.Lcd.drawLine(x, 10, x, 230, WHITE);
-  }
 }
 
 void loop() {}

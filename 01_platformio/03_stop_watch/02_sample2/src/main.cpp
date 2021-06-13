c738fac9f2da40fb7d2637564e472c4b40cb0256
Satoru Osawa <osawa@takram.com>
2021-06-13 13:36:31 +0900

    Refactoring

--- 01_platformio/03_stop_watch/02_sample2/src/main.cpp
+++ 01_platformio/03_stop_watch/02_sample2/src/main.cpp
@@ -9,10 +9,7 @@ float start_sec = 0;
 int center_x = 160;
 int center_y = 120;
 
-void setup() {
-  M5.begin();
-  M5.Lcd.setTextSize(3);
-}
+void setup() { M5.begin(); }
 
 void buttonManagement() {
   if (is_measuring) {
@@ -39,43 +36,44 @@ void buttonManagement() {
 }
 
 void drawSecond() {
+  int inside_rad = 105;
+  int outside_rad = 110;
   float measure_min = measure_sec / 60;
   float last_measure_min = last_measure_sec / 60;
   if (fmod(measure_min, 1) < fmod(last_measure_min, 1)) {
-    M5.lcd.fillEllipse(center_x, center_y, 100, 100, BLACK);
+    M5.lcd.fillEllipse(center_x, center_y, outside_rad + 1, outside_rad + 1,
+                       BLACK);
   }
-  float div = 100;
+  float div = 120;
   float quantum = floor(fmod(measure_min, 1) * div) / div;
   float quantum_angle = quantum * 2 * M_PI - M_PI / 2.0;
-  int x0 = 90 * cos(quantum_angle) + center_x;
-  int x1 = 95 * cos(quantum_angle) + center_x;
-  int y0 = 90 * sin(quantum_angle) + center_y;
-  int y1 = 95 * sin(quantum_angle) + center_y;
+  int x0 = inside_rad * cos(quantum_angle) + center_x;
+  int x1 = outside_rad * cos(quantum_angle) + center_x;
+  int y0 = inside_rad * sin(quantum_angle) + center_y;
+  int y1 = outside_rad * sin(quantum_angle) + center_y;
   M5.Lcd.drawLine(x0, y0, x1, y1, WHITE);
 }
 
 void drawBelowSecond() {
+  int inside_rad = 95;
+  int outside_rad = 100;
   if (fmod(measure_sec, 1) < fmod(last_measure_sec, 1)) {
-    M5.lcd.fillEllipse(center_x, center_y, 86, 86, BLACK);
+    M5.lcd.fillEllipse(center_x, center_y, outside_rad + 1, outside_rad + 1,
+                       BLACK);
   }
-  float div = 50;
+  float div = 40;
   float quantum = floor(fmod(measure_sec, 1) * div) / div;
   float quantum_angle = quantum * 2 * M_PI - M_PI / 2.0;
-  int x0 = 80 * cos(quantum_angle) + center_x;
-  int x1 = 85 * cos(quantum_angle) + center_x;
-  int y0 = 80 * sin(quantum_angle) + center_y;
-  int y1 = 85 * sin(quantum_angle) + center_y;
+  int x0 = inside_rad * cos(quantum_angle) + center_x;
+  int x1 = outside_rad * cos(quantum_angle) + center_x;
+  int y0 = inside_rad * sin(quantum_angle) + center_y;
+  int y1 = outside_rad * sin(quantum_angle) + center_y;
   M5.Lcd.drawLine(x0, y0, x1, y1, WHITE);
 }
 
 void drawTimeCenter() {
-  int font_width = 18;
-  int digit = 0;
-  if (measure_sec > 1) {
-    digit = (int)log10(measure_sec);
-  }
-  M5.Lcd.setCursor(center_x - (digit + 4) * font_width / 2, 105);
-  M5.Lcd.print(measure_sec);
+  M5.Lcd.setTextDatum(MC_DATUM);
+  M5.Lcd.drawString(String(measure_sec), center_x, center_y, 7);
 }
 
 void loop() {
